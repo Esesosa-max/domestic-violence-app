ad0ca7e3c7523ac457d4f08da16904f9578f36ee
Esesosa-max <75207794+Esesosa-max@users.noreply.github.com>
2023-03-18 11:24:18 +0100

    Add files via upload

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,27 +8,54 @@
 			Cuser = user;
 			console.log(Cuser);
 		});
+		// alert('Navbar is swipeable');
 	});
 </script>
 
-<nav class="purple">
+<nav class="">
 	<div class="nav-wrapper container">
 		<a href="/" class="brand-logo">Suggestly</a>
+
 		<a href="#" data-target="mobile-demo" class="sidenav-trigger"
 			><i class="material-icons">menu</i></a
 		>
+
+		<!-- <a href="/new">New</a> -->
 		<ul class="right hide-on-med-and-down">This website is not optimized for desktop yet!</ul>
 	</div>
 </nav>
 
 <ul class="sidenav" id="mobile-demo">
 	{#if Cuser}
-		<li><a href="/department">Department</a></li>
-		<li><a href="/new">New</a></li>
+		{#if !Cuser.email.includes('admin')}
+			<li>
+				<a
+					href="/new"
+					on:click={() => M.toast({ html: 'Navigation Sucessfull, You can swipe away now' })}>New</a
+				>
+			</li>
+		{/if}
 	{:else}
-		<li><a href="/signup">Sign Up</a></li>
-		<!-- <li><a href="/login">Log In</a></li> -->
+		<li>
+			<a
+				href="/signup"
+				on:click={() => M.toast({ html: 'Navigation Sucessfull, You can swipe away now' })}
+				>Sign Up</a
+			>
+		</li>
+		<li>
+			<a
+				href="/login"
+				on:click={() => M.toast({ html: 'Navigation Sucessfull,You can swipe away now' })}>Log In</a
+			>
+		</li>
 	{/if}
 </ul>
 
 <slot />
+
+<style>
+	nav {
+		background-color: #052f5f;
+	}
+</style>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,55 +1,53 @@
-
 <script>
-    let email;
-    let username;
-    let dept;
-    let password;
-    let level;
-    import {  createUserWithEmailAndPassword,signInWithEmailAndPassword,updateProfile } from "firebase/auth";
-    import {auth,db} from '$lib/firebase.js'
-    import { doc, setDoc } from "firebase/firestore";
-    import { redirect } from '@sveltejs/kit'; 
-  const handleSubmit = (e) => {
-  e.preventDefault()
-  console.log(email,username,dept,password,level)
-  if(email.includes('@mgt.uniben.edu')|| 'admin@gmail.com') {
-  
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-     M.toast({html:'Login success'})
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      alert(errorMessage)
-      // ..
-    });
-  }
- 
-  }
-  </script>
-  
-  
-  <div class="row container">
-      <h5>Login</h5>
-      <form class="col s12" on:submit={handleSubmit}>
-        <div class="row">
-          <div class="input-field col s12">
-            <i class="material-icons prefix">email</i>
-            <input id="icon_prefix" type="text" class="validate" bind:value={email}>
-            <label for="icon_prefix">Email</label>
-  
-          </div>
-     
-          <div class="input-field col s12">
-            <i class="material-icons prefix">pin</i>
-            <input id="icon_telephone" type="password"  bind:value={password}>
-            <label for="icon_telephone">Password</label>
-          </div>
-     
-        
-          <br />
-          <button class="red btn" type="submit">Login</button>
-        </div>
-      </form>
-    </div>
+	let email;
+	let username;
+	let dept;
+	let password;
+	let level;
+	import {
+		createUserWithEmailAndPassword,
+		signInWithEmailAndPassword,
+		updateProfile
+	} from 'firebase/auth';
+	import { auth, db } from '$lib/firebase.js';
+	import { doc, setDoc } from 'firebase/firestore';
+	import { redirect } from '@sveltejs/kit';
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(email, password);
+
+		signInWithEmailAndPassword(auth, email.replace('.', '').trim(''), password)
+			.then((userCredential) => {
+				M.toast({ html: 'Login success' });
+				window.location('/');
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				alert(errorMessage);
+				// ..
+			});
+	};
+</script>
+
+<div class="row container">
+	<h5>Login</h5>
+	<form class="col s12" on:submit={handleSubmit}>
+		<div class="row">
+			<div class="input-field col s12">
+				<i class="material-icons prefix">email</i>
+				<input id="icon_prefix" type="text" class="validate" bind:value={email} />
+				<label for="icon_prefix">Email</label>
+			</div>
+
+			<div class="input-field col s12">
+				<i class="material-icons prefix">pin</i>
+				<input id="icon_telephone" type="password" bind:value={password} />
+				<label for="icon_telephone">Password</label>
+			</div>
+
+			<br />
+			<button class="red btn" type="submit">Login</button>
+		</div>
+	</form>
+</div>
