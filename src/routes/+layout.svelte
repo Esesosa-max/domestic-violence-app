4cd2511a7b25a13444604514144e26f0b64b311e
Esesosa-max <75207794+Esesosa-max@users.noreply.github.com>
2023-03-29 07:51:52 +0100

    Add files via upload

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,27 +8,56 @@
 			Cuser = user;
 			console.log(Cuser);
 		});
+		// alert('Navbar is swipeable');
 	});
 </script>
 
-<nav class="purple">
+<nav class="">
 	<div class="nav-wrapper container">
 		<a href="/" class="brand-logo">Suggestly</a>
-		<a href="#" data-target="mobile-demo" class="sidenav-trigger"
-			><i class="material-icons">menu</i></a
-		>
+		{#if Cuser}
+			{#if !Cuser.email.includes('admin')}
+				<a href="#" data-target="mobile-demo" class="sidenav-trigger"
+					><i class="material-icons">menu</i></a
+				>
+			{/if}
+		{/if}
+
+		<!-- <a href="/new">New</a> -->
 		<ul class="right hide-on-med-and-down">This website is not optimized for desktop yet!</ul>
 	</div>
 </nav>
 
+<a class="btn relative red" on:click={() => window.history.back()}>
+	<i class="large material-icons">keyboard_backspace</i>
+</a>
+
 <ul class="sidenav" id="mobile-demo">
 	{#if Cuser}
-		<li><a href="/department">Department</a></li>
-		<li><a href="/new">New</a></li>
+		{#if !Cuser.email.includes('admin')}
+			<li>
+				<a class="sidenav-close" href="/new">New</a>
+			</li>
+		{/if}
 	{:else}
-		<li><a href="/signup">Sign Up</a></li>
-		<!-- <li><a href="/login">Log In</a></li> -->
+		<li>
+			<a href="/signup" class="sidenav-close">Sign Up</a>
+		</li>
+		<li>
+			<a href="/login" class="sidenav-close">Log In</a>
+		</li>
 	{/if}
 </ul>
 
 <slot />
+
+<style>
+	nav {
+		background-color: #052f5f;
+	}
+	.relative {
+		position: absolute;
+		top: 10px;
+		right: 20px;
+	}
+</style>
