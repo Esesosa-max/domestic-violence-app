f32f603a4f4650ad91529ac3b5d8e5e0b1b95729
Esesosa-max <75207794+Esesosa-max@users.noreply.github.com>
2023-03-31 08:59:36 +0100

    Add files via upload

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,27 +8,67 @@
 			Cuser = user;
 			console.log(Cuser);
 		});
+		// alert('Navbar is swipeable');
 	});
 </script>
 
-<nav class="purple">
+<nav class="">
 	<div class="nav-wrapper container">
 		<a href="/" class="brand-logo">Suggestly</a>
-		<a href="#" data-target="mobile-demo" class="sidenav-trigger"
-			><i class="material-icons">menu</i></a
-		>
+		{#if !Cuser}
+			<a href="#" data-target="mobile-demo" class="sidenav-trigger"
+				><i class="material-icons">menu</i></a
+			>
+		{/if}
+
+		<!-- <a href="/new">New</a> -->
 		<ul class="right hide-on-med-and-down">This website is not optimized for desktop yet!</ul>
 	</div>
 </nav>
 
+<a class="btn relative red" on:click={() => window.history.back()}>
+	<i class="large material-icons">keyboard_backspace</i>
+</a>
+
 <ul class="sidenav" id="mobile-demo">
 	{#if Cuser}
-		<li><a href="/department">Department</a></li>
-		<li><a href="/new">New</a></li>
+		{#if !Cuser.email.includes('admin')}
+			<li>
+				<a class="sidenav-close" href="/new">New</a>
+			</li>
+		{/if}
 	{:else}
-		<li><a href="/signup">Sign Up</a></li>
-		<!-- <li><a href="/login">Log In</a></li> -->
+		<li>
+			<a href="/signup" class="sidenav-close">Sign Up</a>
+		</li>
+		<li>
+			<a href="/login" class="sidenav-close">Log In</a>
+		</li>
 	{/if}
 </ul>
+{#if Cuser}
+	{#if !Cuser.email.includes('admin')}
+		<div class="fixed-action-btn">
+			<a class="btn-floating btn-large red" href="/new">
+				<i class="large material-icons">add_circle</i>
+			</a>
+			<!-- <a class="btn tooltipped" data-position="bottom" data-tooltip="I am a tooltip">Make a New Suggestion</a> -->
+		</div>
+	{/if}
+{/if}
 
 <slot />
+
+<style>
+	nav {
+		background-color: #052f5f;
+	}
+	.relative {
+		position: absolute;
+		top: 10px;
+		right: 20px;
+	}
+	.btn-large i {
+		font-size: 2.6rem;
+	}
+</style>
--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -1,87 +1,86 @@
 <script>
-let title;
-let body;
-let userInfo = false
-let Cuser;
-let user2;
-import { collection, addDoc,serverTimestamp,doc,getDoc } from "firebase/firestore"; 
-import {auth,db} from '$lib/firebase.js'
-	import { onMount } from "svelte";
-	import { onAuthStateChanged } from "firebase/auth";
-onMount(async () =>{
-  onAuthStateChanged(auth, (user) =>{
-Cuser = user.email
-user2= user
-console.log(Cuser,user)
-        })
-})
-const handleSubmit = async (e) =>{
-  e.preventDefault();
-   if(Cuser == 'admin@gmail.com'){
-    alert("sorry admins can't make suggestions")
-    return
-   }
-   else{
-   
-console.log(Cuser)
-// debugger;
-  if(title && body){
-  
-
-    const docRef = await addDoc(collection(db, user2.displayName), {
-      title,
-      body,
-      IsAnonoymous:userInfo,
-      currentUser:Cuser,
-      timestamp:serverTimestamp(),
-      dept:user2.displayName
-      });
-      M.toast({html:"Suggestion Recorded Successfully"})
-}  else{
-    alert('please put a title and body')
-  }
-   }
-
-  }
-
-
-  
+	let title;
+	let body;
+	let userInfo = false;
+	let Cuser;
+	let user2;
+	import { collection, addDoc, serverTimestamp, doc, getDoc } from 'firebase/firestore';
+	import { auth, db } from '$lib/firebase.js';
+	import { onMount } from 'svelte';
+	import { onAuthStateChanged } from 'firebase/auth';
+	onMount(async () => {
+		onAuthStateChanged(auth, (user) => {
+			Cuser = user.email;
+			user2 = user;
+			console.log(Cuser, user);
+		});
+	});
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		if (Cuser == 'admin@gmail.com') {
+			alert("sorry admins can't make suggestions");
+			return;
+		} else {
+			console.log(Cuser);
+			// debugger;
+			if (title && body) {
+				const docRef = await addDoc(collection(db, user2.displayName), {
+					title,
+					body,
+					IsAnonoymous: userInfo,
+					currentUser: Cuser,
+					timestamp: serverTimestamp(),
+					dept: user2.displayName
+				});
+				M.toast({ html: 'Suggestion Recorded Successfully' });
+				window.location.href = '/';
+			} else {
+				alert('please put a title and body');
+			}
+		}
+	};
 </script>
 
+{#if user2 && user2.emailVerified}
+	<div class="container">
+		<!-- <i class="material-icons">arrow_back</i> -->
+		<h5>Add a new suggestion</h5>
 
-<div class="container">
-  <h5>Add a new suggestion</h5>
-
-<div class="row">
-    <form class="col s12" on:submit={handleSubmit}>
-      <div class="row">
-        <div class="input-field col s12">
-          <i class="material-icons prefix">title</i>
-          <input id="icon_prefix" type="text" class="validate" bind:value={title}>
-          <label for="icon_prefix">Suggestion Title</label>
-        </div>
-        <div class="input-field col s12">
-            <div class="input-field col s12">
-              <i class="material-icons prefix">B</i>
-                <textarea id="textarea1" class="materialize-textarea" bind:value={body}></textarea>
-                <label for="textarea1">Suggestion body</label>
-              </div>
-
-        </div>
-      </div>
-      <label>
-        <input type="checkbox" bind:checked={userInfo} />
-        <span>Enable Anonymous Mode? it hides your email and username</span>
-      </label>
-      <br />
-      <br />
-      <br />
-      <button class="blue btn" type="submit">Submit</button>
-    </form>
-  </div>
-        
-</div>
+		<div class="row">
+			<form class="col s12" on:submit={handleSubmit}>
+				<div class="row">
+					<div class="input-field col s12">
+						<i class="material-icons prefix">title</i>
+						<input id="icon_prefix" type="text" class="validate" bind:value={title} />
+						<label for="icon_prefix">Suggestion Title</label>
+					</div>
+					<div class="input-field col s12">
+						<div class="input-field col s12">
+							<i class="material-icons prefix">B</i>
+							<textarea id="textarea1" class="materialize-textarea" bind:value={body} />
+							<label for="textarea1">Suggestion body</label>
+						</div>
+					</div>
+				</div>
+				<label>
+					<input type="checkbox" bind:checked={userInfo} />
+					<span>Enable Anonymous Mode? it hides your email and username</span>
+				</label>
+				<br />
+				<br />
+				<br />
+				<button class="blue btn" type="submit">Submit</button>
+			</form>
+		</div>
+	</div>
+{:else}
+	<div class="container">
+		<p>Please Verify your Email</p>
+	</div>
+{/if}
 
 <style>
-  
+	.container {
+		overflow-x: hidden;
+	}
 </style>
